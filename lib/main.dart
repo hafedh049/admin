e49d4ee075711d8b1126b3c708db68da9e8b69e7
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-05-25 15:18:17 +0100

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,21 @@ import 'package:dabka/views/holder.dart';
 import 'package:dabka/views/sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localization/flutter_localization.dart';
 import 'package:get/get.dart';
 
 import 'translations/translation.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(Main());
+  runApp(const Main());
 }
 
 class Main extends StatelessWidget {
-  Main({super.key});
-  final FlutterLocalization localization = FlutterLocalization.instance;
+  const Main({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      supportedLocales: localization.supportedLocales,
-      localizationsDelegates: localization.localizationsDelegates,
       locale: const Locale('ar', 'AR'),
       fallbackLocale: const Locale('ar', 'AR'),
       translations: Translation(),
